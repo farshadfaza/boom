763e61ed38496b19522e428f010a95377655d0bb
farshadfaza <farshadfazafree@gmail.com>
2017-02-05 16:58:39 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -84,7 +84,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {193704183},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
